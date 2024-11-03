01c4a8e402d0420750d9c8656744a7bdb3c18000
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-11-03 19:29:08 -0300

    feat: button set included.

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,33 @@ function App() {
   return (
     <main className='calculator'>
 
-      <Display expression='1+1'/>
+      <Display expression='0'/>
 
       <div className="button-set">
-        <div className="button">7</div>
-        <div className="button">8</div>
-        <div className="button">9</div>
-        <div id="btnAC" className="button">AC</div>
-        <div id="btnDivide" className="button">&divide;</div>
 
-        <div className="button">4</div>
-        <div className="button">5</div>
-        <div className="button">6</div>
-        <div id="btnOpposite" className="button">+/-</div>
-        <div id="btnTimes" className="button">&times;</div>
+        <div className="numeral">7</div>
+        <div className="numeral">8</div>
+        <div className="numeral">9</div>
+        <div id="btnAC" className="misc">AC</div>
+        <div id="btnDivide" className="operator">&divide;</div>
 
-        <div className="button">1</div>
-        <div className="button">2</div>
-        <div className="button">3</div>
-        <div id="btnPercentage" className="button">&#37;</div>
-        <div id="btnMinus" className="button">&minus;</div>
+        <div className="numeral">4</div>
+        <div className="numeral">5</div>
+        <div className="numeral">6</div>
+        <div id="btnOpposite" className="misc">+/-</div>
+        <div id="btnTimes" className="operator">&times;</div>
 
-        <div id="btnZero" className="button">0</div>
-        <div className="button">.</div>
-        <div id="btnEqual" className="button">=</div>
-        <div id="btnPlus" className="button">+</div>
+        <div className="numeral">1</div>
+        <div className="numeral">2</div>
+        <div className="numeral">3</div>
+        <div id="btnPercentage" className="operator">&#37;</div>
+        <div id="btnMinus" className="operator">&minus;</div>
+
+        <div id="btnZero" className="numeral">0</div>
+        <div className="numeral">.</div>
+        <div id="btnEqual" className="operator">=</div>
+        <div id="btnPlus" className="operator">+</div>
+
       </div>
 
     </main>
